fb077b9ed73aea75eb43b02d0bd803365e5eb8d8
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2018-03-29 16:07:00 +0300

    xp bar and level elements for ui and code

--- Assets/Scripts/InstrumentBase.cs
+++ Assets/Scripts/InstrumentBase.cs
@@ -1,31 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InstrumentBase : MonoBehaviour {
 
+    public Slider xpBar;
+    public Text currLvl;
+    public Text nxtLvl;
 
     float exp;
     float expToNext = 15;
-    float level;
+    float level = 1;
+    float nextLevel = 2;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        xpBar.maxValue = expToNext;
+        currLvl.text = "" + level;
+        nxtLvl.text = "" + nextLevel;
+        nextLevel = level + 1.0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        
+        xpBar.value = exp;
+        nxtLvl.text = "" + nextLevel;
+        currLvl.text = "" + level;
 
         if (exp >= expToNext)
         {
             level += 1;
+            nextLevel += 1;
             expToNext = expToNext*2.5f;
-            print("lvl up");
-            print("nex lvl at "+ expToNext);
+            xpBar.minValue = exp;
+            xpBar.maxValue = expToNext;
         }
 	}
 

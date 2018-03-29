13fcaaa401460d112c6441c963419bcf4264415d
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2018-03-29 10:53:57 +0300

    Touch controls

---
+++ Assets/Scripts/TouchControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour {
+
+    Vector3 touchPosWorld;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+
+            RaycastHit2D hitInfo = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+            if (hitInfo.collider != null)
+            {
+                GameObject touchedObject = hitInfo.transform.gameObject;
+                Debug.Log("touched" + touchedObject.transform.name);
+            }
+
+        }
+    }
+}

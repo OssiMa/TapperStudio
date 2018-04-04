2b310ee41ca47a07c570eab5e811a63dad7f9f6a
Siukola Henry TTK16SP <henrysiukola@kamk.fi>
2018-04-04 12:16:47 +0300

    Managing project scenes and making adjustments to create better structure. Making early 'versions' of to be save scripts

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSettings : MonoBehaviour {
+
+    [SerializeField]
+    private Toggle toggle;
+    [SerializeField]
+    private AudioSource myAudio;
+
+    public void Awake()
+    {
+        // 1
+        if (!PlayerPrefs.HasKey("music"))
+        {
+            PlayerPrefs.SetInt("music", 1);
+            toggle.isOn = true;
+            myAudio.enabled = true;
+            PlayerPrefs.Save();
+        }
+        // 2
+        else
+        {
+            if (PlayerPrefs.GetInt("music") == 0)
+            {
+                myAudio.enabled = false;
+                toggle.isOn = false;
+            }
+            else
+            {
+                myAudio.enabled = true;
+                toggle.isOn = true;
+            }
+        }
+    }
+    //A menu and lots of other stuff needs to be done before it makes any sense to complete this script
+}

6d08ccea18a52827ee51c9fcc63ffea9b31e8493
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2018-04-04 14:07:10 +0300

    starting to form drum mechanics

--- Assets/Scripts/Drums.cs
+++ Assets/Scripts/Drums.cs
@@ -13,20 +13,33 @@ public class Drums : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        WithForeachLoop();
+        WithForLoop();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         RotationTimer -= Time.deltaTime;
         if (RotationTimer <= 0)
         {
-
+            NewRotation();
         }
 	}
     void NewRotation()
     {
         RotationTimer = 5;
-        //
+        //thing
+    }
+    void WithForeachLoop()
+    {
+        foreach (Transform child in transform)
+            print("Foreach loop: " + child);
+    }
+
+    void WithForLoop()
+    {
+        int children = transform.childCount;
+        for (int i = 0; i < children; ++i)
+            print("For loop: " + transform.GetChild(i));
     }
 }

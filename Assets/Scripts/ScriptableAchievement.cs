04d14f08af34ba3adf587b153515df95ab5c97cc
Siukola Henry TTK16SP <henrysiukola@kamk.fi>
2018-05-03 14:31:17 +0300

    early achievement stuff

---
+++ Assets/Scripts/ScriptableAchievement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableAchievement", menuName = "Achievement/New Achievement")]
+public class ScriptableAchievement : ScriptableObject
+{
+
+    [HideInInspector]
+    public Sprite AchievementIcon;
+
+    public Color32 color;
+
+    public bool trinket = false;
+
+    [HideInInspector]
+    public bool unlocked = false;
+
+}
